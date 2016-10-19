6cd7f548bb114c6ae26e5565df7a997ae210a1c3
ken88ling <ken88ling@gmail.com>
2016-10-19 11:04:55 +1100

    Initial

---
+++ src/ScheduleEvent/Models/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleEvent.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartAt { get; set; }
+        public DateTime EndAt { get; set; }
+        public bool IsFullDay { get; set; }
+    }
+}
+
