2065887435daa9f3a9dcbc6a3fb22d5a12c184fd
ken88ling <ken88ling@gmail.com>
2016-10-19 11:42:25 +1100

    Dependency inject - using bower Angular fullcalendar moment

--- src/ScheduleEvent/wwwroot/_references.js
+++ src/ScheduleEvent/wwwroot/_references.js
@@ -4,3 +4,8 @@
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
+/// <reference path="lib/fullcalendar/dist/fullcalendar.js" />
+/// <reference path="lib/angular/angular.js" />
+/// <reference path="lib/moment/moment.js" />
+
+
